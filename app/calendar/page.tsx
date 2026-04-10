1a2c1c83a50092c9af7b99bbf2c4ff1ac3c99222
RenaldKS <109418521+RenaldKS@users.noreply.github.com>
2026-04-10 10:40:51 +0700

    New Project
    Created to learn about next.js and for rental website services

---
+++ app/calendar/page.tsx
@@ -0,0 +1,13 @@
+export default function Calendar() {
+  return (
+    <>
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Europe%2FLondon&showPrint=0&showTabs=0&src=Y18yMGM3MWRmOWRlZjc4YTFmZDhkZDdiZGM5NjQ2MDY5NWVkYTk2MGJjZGQyM2NlY2MzZWEwNjc4NTA1NDIzZTdmQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4uY2hyaXN0aWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uaGluZHVpc20jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4uaW5kb25lc2lhbiNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4ua2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4ubmcjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4udGgjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4uanVkYWlzbSNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4uaXNsYW1pYyNob2xpZGF5QGdyb3VwLnYuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4ub3J0aG9kb3hfY2hyaXN0aWFuaXR5I2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%237cb342&color=%23d81b60&color=%23b39ddb&color=%230b8043&color=%23616161&color=%23795548&color=%233f51b5&color=%23f4511e&color=%23f6bf26&color=%234285f4"
+        width="90%"
+        height="700"
+        frameBorder="0"
+        scrolling="no"
+      ></iframe>
+    </>
+  );
+}
---
+++ app/kontrakan/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+export default function kontrakan() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center justify-between gap-8 px-6 py-12 mx-auto md:flex-row max-w-7xl relative container ">
+        <div className="flex-1">
+          <h1 className="font-bold text-6xl px-2">Kontrakan Senang Hati</h1>
+          <p className="text-sm text-gray-600 uppercase px-2 py-5">
+            Escape to this cozy Nordic cabin, perfect for solo retreats or group
+            getaways — whether it is unwinding by the fire after a day of skiing
+            or enjoying a morning coffee with stunning views.
+          </p>
+        </div>
+        <Image
+          src="/kontrakan1.jpg"
+          alt="image here"
+          width={700}
+          height={250}
+        />
+      </section>
+      <div className="hidden md:block w-full h-px bg-slate-200/50 mx-auto max-w-2xl shadow-xs"></div>
+      <section
+        id="gallery"
+        className="flex flex-row items-center justify-center justify-between gap-8 px-6 py-12 mx-auto max-w-7xl relative container "
+      >
+        <div className="flex-1">
+          <h1 className="font-bold text-6xl px-2">Galeri</h1>
+        </div>
+      </section>
+    </>
+  );
+}
